93545b7a560860ab21792fcc9402df504503370b
ssdcfxb <778911093@qq.com>
2025-08-31 20:53:10 +0800

    [feat] first commit

---
+++ auto_robot/launch/auto_robot.launch.py
@@ -0,0 +1,51 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.conditions import IfCondition, UnlessCondition
+
+import xacro
+
+
+def generate_launch_description():
+
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('auto_robot'))
+    xacro_file = os.path.join(pkg_path,'robots','auto_robot.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    
+    
+    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        condition=UnlessCondition(LaunchConfiguration('false'))
+    )
+    
+    
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[params]
+    )
+
+
+    # Launch!
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+
+        node_robot_state_publisher
+    ])
---
+++ auto_robot/launch/load_urdf_into_gazebo.launch.py
@@ -0,0 +1,107 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_path
+from launch.actions import DeclareLaunchArgument
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='auto_robot' #<--- CHANGE ME
+    world_file_path = 'worlds/indoor.world'
+    urdf_tutorial_path = get_package_share_path('auto_robot')
+    pkg_path = os.path.join(get_package_share_directory(package_name))
+    world_path = os.path.join(pkg_path, world_file_path)
+    default_rviz_config_path = urdf_tutorial_path / 'rviz/urdf.rviz'
+
+    # Pose where we want to spawn the robot
+    spawn_x_val = '0.0'
+    spawn_y_val = '0.0'
+    spawn_z_val = '0.1'
+    spawn_yaw_val = '0.0'
+
+    auto_robot = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','auto_robot.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+                                     description='Absolute path to rviz config file')
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    #          )
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+                launch_arguments={'gz_args': " -r -v 1 empty.sdf"}.items()
+             )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(
+        package='ros_gz_sim', 
+        executable='create',
+        arguments=['-topic', 'robot_description',
+                   '-name', 'auto_robot',
+                   '-x', spawn_x_val,
+                   '-y', spawn_y_val,
+                   '-z', spawn_z_val,
+                   '-Y', spawn_yaw_val],
+        output='screen'
+    )
+
+    # 添加控制器管理器
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            os.path.join(get_package_share_directory(package_name), "config", "chassis_controllers.yaml"),
+            os.path.join(get_package_share_directory(package_name), "config", "z1_controllers.yaml")
+        ],
+        output="screen",
+    )
+
+    # 启动关节状态广播器
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    # 启动 Z1 位置控制器
+    z1_position_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["position_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        auto_robot,
+        gazebo,
+        rviz_arg,
+        spawn_entity,
+        controller_manager,
+        joint_state_broadcaster_spawner,
+        z1_position_controller_spawner,
+        rviz_node
+    ])
